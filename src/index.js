3131229ddd016ddb42b400d0239b83405adfc4c8
FredNo94 <galenkov9@gmail.com>
2024-05-06 20:06:34 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import _ from "lodash";
 export  default function consolePrint() {
     console.log('Hello, Hexlet');
     console.log(_.last(['one', 'two']));
+    console.log('Hello, Sashka');
 };
 
